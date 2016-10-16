c91f0746863f5270a9dda20cfbb3af5d80e2de3a
Tomasz Balon <tomasz.balon@lunarlogic.io>
2016-10-16 14:45:12 +0200

    Adding new track

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,4 +2,15 @@ class TracksController < ApplicationController
   def index
     @tracks = Track.all
   end
+
+  def create
+    Track.create!(track_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:link)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "tracks#index"
+  resources :tracks, only: [:create]
 end
